7b7654b60eb870119b2331e46b97b6b5cb137781
Antonio Luque Bravo <toniluque097@gmail.com>
2017-08-12 20:51:37 +0200

    Añadida funcionalidad de Registro y Login

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -5,6 +5,8 @@
   use Symfony\Component\HttpFoundation\Request;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
   use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+  use AppBundle\Entity\User;
+  use AppBundle\Form\UserType;
 
 
   class SecurityController extends Controller
@@ -15,6 +17,7 @@
      */
      public function loginAction(Request $request)
      {
+        $authUtils = $this->get('security.authentication_utils');
         // get the login error if there is one
         $error = $authUtils->getLastAuthenticationError();
 
@@ -26,5 +29,32 @@
             'error'         => $error,
         ));
      }
+    /**
+     * @Route("/register", name="register")
+     */
+     public function registerAction(Request $request)
+     {
+       $em = $this->getDoctrine()->getManager();
+       $user = new User();
+       $form = $this->createForm(UserType::class, $user);
+       $form->handleRequest($request);
+       if ($form->isSubmitted() && $form->isValid()) {
+         $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPlainPassword());
+         $user->setPassword($password);
+         $em->persist($user);
+         $em->flush();
+         return $this->redirectToRoute('login');
+       }
+       return $this->render('security/register.html.twig', array(
+         'form' => $form->createView()
+       ));
+     }
+     /**
+      * @Route("/logout", name="logout")
+      */
+      public function logoutAction(Request $request)
+      {
+      // UNREACHABLE CODE
+      }
   }
 ?>
